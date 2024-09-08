67af95753c5415925a9e49676330e0077f6f7dc0
Hp <ahsanali.official085@gmail.com>
2024-09-08 15:20:30 +0500

    Added vercel.json file

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
+import express from 'express';
+const app = express()
 import Stripe from 'stripe';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const stripe = new Stripe(process.env.SECRET_KEY);
+const stripe = new Stripe(process.env.SECRET_KEY)
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const customer = await stripe.customers.create();
-      const ephemeralKey = await stripe.ephemeralKeys.create(
-        { customer: customer.id },
-        { apiVersion: '2023-10-16' }
-      );
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: req.body.amount,
-        currency: 'usd',
-        customer: customer.id,
-        automatic_payment_methods: {
-          enabled: true,
+
+
+const port = process.env.PORT;
+
+
+app.post('/payment/intents',async(req,res)=>{
+    const customer =  await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        {customer: customer.id},
+        {apiVersion: '2023-10-16'}
+    );
+   try{
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount:req.body.amount,
+        currency:'usd',
+        customer:customer.id,
+        automatic_payment_methods:{
+            enabled: true,
         },
-      });
-      res.json({
-        paymentIntent: paymentIntent.client_secret,
-        ephemeralKey: ephemeralKey.secret,
-        customer: customer.id,
-      });
-    } catch (error) {
-      res.status(400).json({
-        error: error.message,
-      });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
+    });
+    res.json({
+        paymentIntent:paymentIntent.client_secret,
+        ephemeralKey:ephemeralKey.secret,
+        customer:customer.id,
+    })
+   }catch(error){
+    res.status(400).json({
+        error:error.message,
+    })
+   }
+})
+
+app.listen(port,()=>{
+    console.log(`App is listening on port ${port}`)
+})
